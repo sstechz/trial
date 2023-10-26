bab2114205ea367dc6b9d57d0b5e78f338c731dc
Shrey <shreysahay1@gmail.com>
2023-10-26 13:47:40 +0530

    renamed the link

--- server3/index.js
+++ server3/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://glistening-wisp-962ec9.netlify.app/",
+    origin: "https://653a20660211586cb1751c0b--quiet-tartufo-935f26.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
